5389c872745507cb96e30d8c606f54ed70aa67c1
Lipski.Mikalai <lipski.mikalai@gmail.com>
2020-10-08 13:14:40 +0300

    tasks_2

---
+++ task_2_1.py
@@ -0,0 +1,20 @@
+str = input ('Enter str : ')
+
+str_1 = str[2]
+print(str_1)
+print('-----------')
+
+str_2 = str[-2]
+print(str_2)
+print('-----------')
+
+str_3 =  str[:5]
+print(str_3)
+print('___________')
+
+str_4 =  str[:-2]
+print(str_4)
+print('-----------')
+
+str_5 = str[::2]
+print(str_5)
