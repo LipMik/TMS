22d0090044290b8a663aa6e5bebe7b6450514d91
Lipski.Mikalai <lipski.mikalai@gmail.com>
2020-10-20 14:40:48 +0300

    task_6_1.py

--- task_6_1.py
+++ task_6_1.py
@@ -67,6 +67,29 @@ def nul_up(matr, x, y):
     return matr
 
 
+def summ_matrix_a_b(a: list, b: list):
+    rezult_matr = [[0 for i in range(len(a))] for j in range(len(a[0]))]
+    for i in range(len(a)):
+        for j in range(len(a[0])):
+            rezult_matr[i][j] = (int(a[i][j]) + int(b[i][j]))
+    return rezult_matr
+
+
+def difference_a_b(a: list, b: list):
+    rezult_matr = [[0 for i in range(len(a))] for j in range(len(a[0]))]
+    for i in range(len(a)):
+        for j in range(len(a[0])):
+            rezult_matr[i][j] = (int(a[i][j]) - int(b[i][j]))
+    return rezult_matr
+
+
+def multiplication(matr_a, g):
+    for i in range(len(matr_a)):
+        for j in range(len(matr_a[0])):
+            matr_a[i][j] *= g
+    return matr_a
+
+
 n = int(input('Enter N: '))
 m = int(input('Enter M: '))
 
@@ -74,34 +97,44 @@ matrix = create_matrix(n, m)
 print(matrix)
 
 summ_matr = summ_matrix(matrix)
-print(summ_matr)
+print('Summa of elements', summ_matr)
 
 max_matrix = max_of_matrix(matrix)
-print(max_matrix)
+print('Max element', max_matrix)
 
 min_matrix = min_of_matrix(matrix)
-print(min_matrix)
+print('Min element', min_matrix)
 
 max_line = ind_max_line(matrix)
-print(max_line)
+print('Index of row with max element', max_line)
 
 min_line = ind_min_line(matrix)
-print(min_line)
+print('Index of line with min element',min_line)
 
 s_column = summ_column(matrix)
-print(s_column)
 
 min_column, max_column = ind_column(s_column)
-print(max_column)
-print(min_column)
+print('Index of column with max element', max_column)
+print('Index of column with min element', min_column)
 
 matrix = nul_down(matrix, n, m)
-print(matrix)
+print('replacing elements below the main diagonal', matrix)
 
 matrix = nul_up(matrix, n, m)
-print(matrix)
+print('replacing elements above the main diagonal', matrix)
 
 matrix_a = create_matrix(n, m)
+print('New matrix A', matrix_a)
+
 matrix_b = create_matrix(n, m)
-print(matrix_a)
-print(matrix_b)
+print('New matrix B', matrix_b)
+
+matrix_c = summ_matrix_a_b(matrix_a, matrix_b)
+print('Summ matrixs A and B', matrix_c)
+
+matrix_c = difference_a_b(matrix_a, matrix_b)
+print('Difference between A and B', matrix_c)
+
+g = int(input('Enter g: '))
+matrix_a = multiplication(matrix_a, g)
+print('matrix_a * g', matrix_a)
