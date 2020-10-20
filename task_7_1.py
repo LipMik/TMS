b47b3ce065f30a4cbfdc44bdd5d9163cbba8dafb
Lipski.Mikalai <lipski.mikalai@gmail.com>
2020-10-20 23:36:40 +0300

    task_7_1.py

---
+++ task_7_1.py
@@ -0,0 +1,35 @@
+i = float(input('Enter value '))
+print((lambda a, dm=2.54: dm * a)(i))
+
+i = float(input('Enter value '))
+print((lambda a, cm=0.39: cm * a)(i))
+
+i = float(input('Enter value '))
+print((lambda a, ml=1.61: ml * a)(i))
+
+i = float(input('Enter value '))
+print((lambda a, km=0.62: km * a)(i))
+
+i = float(input('Enter value '))
+print((lambda a, nt=0.45: nt * a)(i))
+
+i = float(input('Enter value '))
+print((lambda a, kg=2.2: kg * a)(i))
+
+i = float(input('Enter value '))
+print((lambda a, unc=28.35: unc * a)(i))
+
+i = float(input('Enter value '))
+print((lambda a, gr=0.0353: gr * a)(i))
+
+i = float(input('Enter value '))
+print((lambda a, gl=4.55: gl * a)(i))
+
+i = float(input('Enter value '))
+print((lambda a, l_gl=0.22: l_gl * a)(i))
+
+i = float(input('Enter value '))
+print((lambda a, pt=0.57: pt * a)(i))
+
+i = float(input('Enter value '))
+print((lambda a, l_p=1.76: l_p * a)(i))
