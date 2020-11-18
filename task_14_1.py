c6b73e98e04ae80067a89ebdebcb65278ab5ef76
Lipski.Mikalai <lipski.mikalai@gmail.com>
2020-11-18 22:36:17 +0300

    14 branch

---
+++ task_14_1.py
@@ -0,0 +1,25 @@
+import argparse
+from _datetime import timedelta, datetime
+import time
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('user_name', help='Имя пользователя', type=str, default='LMikalai')
+parser.add_argument('start_h', help='Количество часов', type=int, default=0)
+parser.add_argument('start_m', help='Количество минут', type=int, default=0)
+parser.add_argument('start_s', help='Количество секунд', type=int, default=1)
+args = parser.parse_args()
+
+step = timedelta(seconds=1)
+start_time = timedelta(hours=args.start_h, minutes=args.start_m, seconds=args.start_s)
+finish = timedelta(hours=0, minutes=0, seconds=0)
+
+with open('log.txt', 'a+') as log:
+    log.writelines(f'{args.user_name} : {datetime.now()}')
+
+while start_time > finish:
+    print(f'Time : {start_time}')
+    time.sleep(1)
+    start_time -= step
+    if start_time == finish:
+        print('Alarm')
---
+++ task_14_2.py
@@ -0,0 +1,25 @@
+import argparse
+from _datetime import datetime
+import time
+
+parser = argparse.ArgumentParser()
+parser.add_argument('user_name', help='Имя пользователя', type=str, default='Микалай')
+parser.add_argument('last_name', help='Фамилия пользователя', type=str, default='Липский')
+parser.add_argument('pomodoro_m', help='Время работы', type=int, default=25)
+parser.add_argument('pause', help=' Пауза (мин.)', type=int, default=5)
+parser.add_argument('count_of_cycle', help='Количество циклов', type=int, default=4)
+args = parser.parse_args()
+
+with open('log2.txt', 'a+') as log:
+    log.writelines(f'{args.user_name} : {args.last_name} : {datetime.now()}\n')
+
+cycle = 0
+work_time = int(args.pomodoro_m * 60)
+pause_time = int(args.pause * 60)
+
+while cycle < args.count_of_cycle:
+    print('Pomodoro started! Work!')
+    time.sleep(work_time)
+    print('Stop work!Pause...')
+    time.sleep(pause_time)
+    cycle += 1
